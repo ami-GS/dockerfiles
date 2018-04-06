9032b58354df7529730569d616e4b93d5cb4eff9
ami-GS <1991.daiki@gmail.com>
2018-04-06 14:57:36 +0900

    fix device sync problem

--- qat_nginx/docker_run_env_and_attach.sh
+++ qat_nginx/docker_run_env_and_attach.sh
@@ -1,2 +1,2 @@
-docker run -p 8080:80 -p 4433:443 -it --rm --name docker_nginx_asynch -d --cap-add=ALL --privileged $1
+docker run -p 8080:80 -p 4433:443 -v /dev:/dev -it --rm --name docker_nginx_asynch -d --cap-add=ALL --privileged $1
 docker exec -it docker_nginx_asynch /bin/bash
